3433787e0281575dea612a5a0748ad79145422a9
Chris <christianrenner6@gmail.com>
2021-05-11 16:39:35 +0200

    Changed variable Names

--- src/Rental.java
+++ src/Rental.java
@@ -1,10 +1,13 @@
 class Rental {
     private Movie movie;
     private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
+
+    public Rental(Movie newmovie, int newdaysRented)
+    {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
